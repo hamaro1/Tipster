d59f38e69f7b42a80501ec3cb780b02c15a705d2
hamaro01 <Katherine.helena01@gmail.com>
2020-07-22 20:03:39 -0400

    Initial Commit

---
+++ Tipster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by Helen Amaro on 7/22/20.
+//  Copyright © 2020 Helen Amaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
